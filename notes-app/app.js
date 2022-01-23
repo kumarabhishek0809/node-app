73e0a69584e417043c3b72f1306d91817a0674cc
Kumar Abhishek <mail2kumar.2021@gmail.com>
2022-01-23 13:14:19 +0400

    test

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const utils = require('./utils.js')
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
 
 //fs.writeFileSync('notes.txt','This file was created by node.js');
 
@@ -10,3 +11,6 @@ fs.appendFileSync('notes.txt','  Kumar append');
 console.log(utils(2,-4))
 
 console.log(getNotes)
+console.log(chalk.blue('Hello world'))
+
+console.log(process.argv);
