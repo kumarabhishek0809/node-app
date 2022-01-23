71886f49a3af2a17ac9ae7c4420c075a4f5bd64a
Kumar Abhishek <mail2kumar.2021@gmail.com>
2022-01-23 21:16:07 +0400

    test

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,61 @@
-const fs = require('fs');
-const utils = require('./utils.js')
-const getNotes = require('./notes.js');
-const chalk = require('chalk');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-//fs.writeFileSync('notes.txt','This file was created by node.js');
 
-fs.appendFileSync('notes.txt','  Kumar append');
 
-//console.log(sum);
-console.log(utils(2,-4))
 
-console.log(getNotes)
-console.log(chalk.blue('Hello world'))
 
-console.log(process.argv);
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse()
