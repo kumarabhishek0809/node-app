7476aa76bd3014dd7a6e0ee4395bd998319e8136
Kumar Abhishek <mail2kumar.2021@gmail.com>
2022-01-27 23:05:29 +0400

    test

--- notes-app/app.js
+++ notes-app/app.js
@@ -35,8 +35,16 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function () {
+    builder: {
+        title: {
+            describe: "Note Title",
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler: function (argv) {
         console.log('Removing the note')
+        notes.removeNote(argv.title)
     }
 })
 
